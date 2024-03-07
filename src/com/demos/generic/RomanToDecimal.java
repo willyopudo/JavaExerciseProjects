b0b2e3c5fdd00752dace7c12bcedfc2b31b9454a
willyopudo <opudowilfred.ow@gmail.com>
2024-03-07 08:56:18 +0300

    Added Roman number converter

---
+++ src/com/demos/generic/RomanToDecimal.java
@@ -0,0 +1,35 @@
+package com.demos.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToDecimal {
+    public static void main(String[] args) {
+
+        String inputRoman= "LXVII";
+        System.out.println("The Integer value of given Roman number is: " + convertRomanToDecimal(inputRoman));
+    }
+
+    private static int convertRomanToDecimal(String s){
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int sum = 0;
+        for(int i=0;i<s.length();i++){
+            int val1 = romanMap.get(s.charAt(i));
+            if(i+1 < s.length()){
+                int val2 = romanMap.get(s.charAt(i + 1));
+                sum = val1 >= val2 ? sum + val1 : sum - val1;
+            }
+            else
+                sum = sum + val1;
+        }
+        return sum;
+    }
+}
